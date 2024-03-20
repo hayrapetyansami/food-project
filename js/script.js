1ac52bdf825407ec20657b2ba13db7bfd907f16f
hayrapetyansami <samihayrapetyan@gmail.com>
2024-03-20 22:33:13 +0400

    created timer functionnality & logic, and fixed all bugs

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 window.addEventListener("DOMContentLoaded", () => {
+  //  tabs start
   const tabs = document.querySelectorAll(".tabheader__items .tabheader__item");
   const tabsContent = document.querySelectorAll(".tabcontainer .tabcontent");
   const tabsParent = document.querySelector(".tabheader__items");
-  let animate = true;
+  let animate = false;
 
   function hideTabContent() {
     tabsContent.forEach(item => {
@@ -10,9 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
       item.classList.remove("show", `${animate ? "fade" : null}`);
     });
 
-    tabs.forEach(item => {
-      item.classList.remove("tabheader__item_active");
-    });
+    tabs.forEach(item => item.classList.remove("tabheader__item_active"));
   }
 
   function showTabContent(item = 0) {
@@ -34,4 +33,73 @@ window.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+  //  tabs end
+
+  // timer start
+  const deadline = "2024-05-21";
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const total = Date.parse(endtime) - Date.parse(new Date());
+
+    if (total <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+
+      // alternative varfiant
+      // document.querySelector(".timer").innerHTML = `
+      //   <h4
+      //     style="
+      //       text-align: center;
+      //       color: red;
+      //       width: 100%;
+      //       text-transform: uppercase;
+      //       font-size: 34px
+      //     "
+      //   >
+      //     sale is out !
+
+      //   </h4>
+      // `;
+    } else {
+      days = Math.floor(total / (1000 * 60 * 60 * 24));
+      hours = Math.floor((total / 1000 * 60 * 60) % 24);
+      minutes = Math.floor((total / 1000 / 60) % 60);
+      seconds = Math.floor((total / 1000) % 60);
+    }
+
+    return { total, days, hours, minutes, seconds };
+  }
+
+  function setZero(n) {
+    return n >= 0 && n < 10 ? `0${n}` : n;
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const daysElement = timer.querySelector("#days");
+    const hoursElement = timer.querySelector("#hours");
+    const minutesElement = timer.querySelector("#minutes");
+    const secondsElement = timer.querySelector("#seconds");
+    const intervalID = setInterval(updateClock, 1000);
+
+    updateClock();
+    function updateClock() {
+      const { total, days, hours, minutes, seconds } = getTimeRemaining(endtime);
+
+      daysElement.innerHTML = setZero(days);
+      hoursElement.innerHTML = setZero(hours);
+      minutesElement.innerHTML = setZero(minutes);
+      secondsElement.innerHTML = setZero(seconds);
+
+      if (total <= 0) {
+        clearInterval(intervalID);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
+  // timer end
 });

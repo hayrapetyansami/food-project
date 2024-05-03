c993de5ce4b94b7a7d38dd506f96f4ceb137d368
hayrapetyansami <samihayrapetyan@gmail.com>
2024-05-03 22:57:39 +0400

    we created api for menus and added some node modules in our project, now food projects menu working automtaically

--- back/server.js
+++ back/server.js
@@ -1,18 +1,27 @@
 const express = require("express");
+const multer = require("multer")
 const sqlite3 = require("sqlite3");
 const cors = require("cors");
+
 const app = express();
 const PORT = 8888;
 
-app.use(cors(), express.json());
+app.use(cors());
+app.use(express.static("static"));
+app.use(express.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/') // specify your destination folder here
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage });
 
 const DB = new sqlite3.Database("./db.db");
-
-// DB.serialize(() => {
-//   DB.run(
-//     "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT);"
-//   )
-// })
 
 app.post("/data", (req, res) => {
   const { name, phone } = req.body;
@@ -27,8 +36,35 @@ app.post("/data", (req, res) => {
   });
 });
 
+app.post("/upload", upload.single("image"), (req, res) => {
+  const { title, description, price } = req.body;
+  const image = req.file.originalname;
+
+  const insertQuery = "INSERT INTO menu (title, description, price, image) VALUES (?, ?, ?, ?);";
+  DB.run(insertQuery, [title, description, price, image], (err) => {
+    if (err) {
+      console.log(req.body);
+      console.log(req.file);
+      res.status(500).send("DB ERROR");
+    } else {
+      res.status(200).send("All is okay");
+    }
+  });
+});
+
 app.get("/users", (req, res) => {
   const selectQuery = "SELECT * FROM users";
+  DB.all(selectQuery, (err, rows) => {
+    if (err) {
+      res.status(500).send("ERROR IN GET USERS");
+    } else {
+      res.status(200).send(rows);
+    }
+  });
+});
+
+app.get("/menu", (req, res) => {
+  const selectQuery = "SELECT * FROM menu";
   DB.all(selectQuery, (err, rows) => {
     if (err) {
       res.status(500).send("ERROR IN GET USERS");
--- front/js/script.js
+++ front/js/script.js
@@ -188,15 +188,20 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  new MenuCard("img/tabs/vegy.jpg", "vegy", "Меню \"Фитнес\"", `Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих
-            овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной
-            ценой и высоким качеством!`, 8, ".menu__field .container").render();
-  new MenuCard("img/tabs/elite.jpg", "elite", "Меню \"Премиум\"", `В меню "Премиум" мы используем не только красивый дизайн упаковки, но
-            и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода
-            в ресторан!`, 13, ".menu__field .container").render();
-  new MenuCard("img/tabs/post.jpg", "post", "Меню \"Постное\"", `Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие
-            продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное
-            количество белков за счет тофу и импортных вегетарианских стейков.`, 19, ".menu__field .container").render();
+  fetch("http://localhost:8888/menu")
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(item => {
+        new MenuCard(
+          `../back/uploads/${item.image}`,
+          `${item.title}`,
+          `${item.title}`,
+          `${item.description}`,
+          `${item.price}`,
+          ".menu__field .container"
+        ).render();
+      });
+    });
 
   // forms
 
@@ -265,8 +270,4 @@ window.addEventListener("DOMContentLoaded", () => {
       // });
     });
   }
-
-  fetch("http://localhost:8888/users")
-    .then(response => response.json())
-    .then(data => console.log(data));
 });
